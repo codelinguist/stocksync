97bead920c78e12524d8612616e2b7aa5a48c537
RJ <rbarro@disa.com>
2026-08-12 20:04:44 +0800

    Create project

---
+++ src/test/java/org/dropship/stocksync/StocksyncApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dropship.stocksync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
